66d742987cecfb32d1b427f7f4db4e5673f641d0
yusniii <83091198+yusniii@users.noreply.github.com>
2023-11-22 11:01:11 +0800

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -32,14 +32,14 @@ from . import _kazu_cache
 
 SUP_BUTTONS = [
     [
-        Button.url("• Repo •", url="https://github.com/ionmusic/Kazu-Ubot"),
-        Button.url("• Support •", url="t.me/kazusupportgrp"),
+        Button.url("• Repo •", url="https://github.com/yusniii/Yxzn-Ubot"),
+        Button.url("• Support •", url="t.me/storeyxzn"),
     ],
 ]
 
 ofox = "https://graph.org/file/231f0049fcd722824f13b.jpg"
 gugirl = "https://graph.org/file/0df54ae4541abca96aa11.jpg"
-kazupic = "https://graph.org//file/d854abd533a783c6642b1.jpg"
+kazupic = "https://telegra.ph/file/7dff64eb30c82741ab9f5.jpg"
 
 apis = [
     "QUl6YVN5QXlEQnNZM1dSdEI1WVBDNmFCX3c4SkF5NlpkWE5jNkZV",
@@ -169,10 +169,10 @@ async def _(e):
 async def repo(e):
     res = [
         await e.builder.article(
-            title="Kazu Ubot",
+            title="Yxzn Ubot",
             description="Ubot | Telethon",
             thumb=wb(kazupic, 0, "image/jpeg", []),
-            text="**◈ ᴋᴀᴢᴜ ᴜʙᴏᴛ​ ◈**",
+            text="**◈ YXZN UBOT​ ◈**",
             buttons=SUP_BUTTONS,
         ),
     ]
